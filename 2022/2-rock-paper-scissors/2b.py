07b71b1061663cb8f6aec8b606d6224525ea5860
Jeremy Payne <jeremypayne26@gmail.com>
2022-12-01 23:03:59 -0700

    Day 2

---
+++ 2022/2-rock-paper-scissors/2b.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import fileinput
+
+total = 0
+for line in fileinput.input():
+    # convert plays to their respective point values
+    opponent = ord(line[0]) - 64
+    me = ((((ord(line[2]) - 89) + (opponent - 1)) + 3) % 3) + 1  # lol
+
+    if opponent == me:  # draw
+        round_score = 3 + me
+    elif me - opponent == 1 or me - opponent == -2:  # win
+        round_score = 6 + me
+    else:  # loss
+        round_score = me
+    total += round_score
+
+print(total)
