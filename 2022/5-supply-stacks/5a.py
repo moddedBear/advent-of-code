01deb0c54f7b39d44a9508aa02051b16f70bdb17
Jeremy Payne <jeremypayne26@gmail.com>
2022-12-04 23:58:41 -0700

    Day 5

---
+++ 2022/5-supply-stacks/5a.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import sys
+from collections import deque
+
+filename = sys.argv[1]
+
+# parse stack layout
+stacks = None
+with open(filename, 'r') as f:
+    for line in f:
+        if not stacks:
+            stacks = [deque() for _ in range(len(line) // 4)]
+        if str.isdigit(line[1]):
+            break
+        for i in range(len(line) // 4):
+            crate = line[i*4+1]
+            if crate != ' ':
+                stacks[i].appendleft(crate)
+
+    # run instructions
+    for line in f:
+        if line[0] == '\n':
+            continue
+        line_list = line.split()
+        quantity = int(line_list[1])
+        from_stack = int(line_list[3]) - 1
+        to_stack = int(line_list[5]) - 1
+        for i in range(quantity):
+            temp = stacks[from_stack].pop()
+            stacks[to_stack].append(temp)
+
+# get answer
+result = []
+for stack in stacks:
+    result.append(stack[-1])
+result = ''.join(str(x) for x in result)
+print(result)
