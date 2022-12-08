fc92366abfe5ba2f947d00b6ecd6112e7adae788
Jeremy Payne <jeremypayne26@gmail.com>
2022-12-07 19:26:19 -0700

    Finish Day 7 overengineered Go solution

--- 2022/7-no-space-left-on-device/7.go
+++ 2022/7-no-space-left-on-device/7.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -10,6 +11,8 @@ import (
 
 var root *dir
 var currentDir *dir
+var part1 int = 0
+var part2 int = math.MaxInt
 
 type file struct {
 	name     string
@@ -35,7 +38,11 @@ type dir struct {
 }
 
 func newDir(name string) *dir {
-	d := dir{name: name}
+	d := dir{
+		name:  name,
+		files: make(map[string]*file),
+		dirs:  make(map[string]*dir),
+	}
 	return &d
 }
 
@@ -65,6 +72,34 @@ func parseFile(lineSlice []string) {
 	}
 }
 
+// recursively calculates and saves size of a directory
+func findSize(d *dir) int {
+	result := 0
+	for _, v := range d.dirs {
+		result += findSize(v)
+	}
+	for _, v := range d.files {
+		result += v.size
+	}
+	d.size = result
+	return result
+}
+
+// finds answers for part1 and part2
+func solve(d *dir, spaceToFree int) {
+	for _, v := range d.dirs {
+		solve(v, spaceToFree)
+	}
+	// part 1
+	if d.size <= 100000 {
+		part1 += d.size
+	}
+	// part 2
+	if d.size >= spaceToFree && d.size < part2 {
+		part2 = d.size
+	}
+}
+
 func main() {
 	filename := os.Args[1]
 
@@ -79,5 +114,18 @@ func main() {
 	for scanner.Scan() {
 		line := strings.Trim(scanner.Text(), "\n ")
 		lineSlice := strings.Split(line, " ")
+		if lineSlice[0] == "$" {
+			if lineSlice[1] == "cd" {
+				Cd(lineSlice[2])
+			}
+		} else {
+			parseFile(lineSlice)
+		}
 	}
+
+	// dfs to find size
+	findSize(root)
+	spaceToFree := 30000000 - (70000000 - root.size)
+	solve(root, spaceToFree)
+	fmt.Printf("Part 1: %v\nPart 2: %v\n", part1, part2)
 }
